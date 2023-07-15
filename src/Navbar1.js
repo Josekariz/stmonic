566fdba4cf847b4a0bde81b412a91ce56849b8c8
Joseph <sejokarizz@gmail.com>
2023-07-16 00:38:43 +0300

    make navbar responsive

--- src/Navbar1.js
+++ src/Navbar1.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar1.css";
 
 const Navbar1 = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav>
-      <ul className="nav-links">
+    <nav className={`navbar ${isOpen ? "open" : ""}`}>
+      <div className="logo">
+        {/* <img src={"https://placehold.co/70"} alt="Logo" /> */}
+      </div>
+      <ul className={`nav-links ${isOpen ? "open" : ""}`}>
         <li>
-          <Link to="/">Home </Link>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/blaze">Blaze </Link>
+          <Link to="/blaze">Blaze</Link>
         </li>
         <li>
-          <Link to="/events">Events </Link>
+          <Link to="/events">Events</Link>
         </li>
         <li>
           <Link to="/about">About</Link>
         </li>
       </ul>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
     </nav>
   );
 };
