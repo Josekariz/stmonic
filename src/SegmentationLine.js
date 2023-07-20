415c0bd9d511cc4ca793468677ea526475f57010
Joseph <sejokarizz@gmail.com>
2023-07-20 16:07:27 +0300

    create segmentation component with linear grad

---
+++ src/SegmentationLine.js
@@ -0,0 +1,9 @@
+// SegmentationLine.js
+import React from "react";
+import "./SegmentationLine.css"; // Import the SegmentationLine.css file
+
+const SegmentationLine = () => {
+  return <hr className="segmentation-line" />;
+};
+
+export default SegmentationLine;
