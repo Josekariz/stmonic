6a5e4da77026b13c82202d38370a769f0e7f95ce
Joseph <sejokarizz@gmail.com>
2023-07-23 08:21:16 +0300

    create google calendar component  for events

--- src/Events.js
+++ src/Events.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Navbar1 from "./Nav/Navbar1";
+import "./events.css"; // Import the events.css file
 
 const Events = () => {
   return (
     <>
       <Navbar1 />
       <h1>Welcome to the Events Page</h1>
+      <div className="calendar-container">
+        <iframe
+          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%234285F4&ctz=Africa%2FNairobi&showTz=0&showTabs=1&showCalendars=1&src=YWIzNzYwMjYxODNlNzY0MmQ1Njc5ZjZlOGNlNGQ5ZmZjMjIyNmQ2ZmE3MzBiY2QwMmMxOWM2ZDJhNjQ5ZGY5YkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4ua2UjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23AD1457&color=%230B8043"
+          title="Church Events Calendar"
+          className="calendar-iframe" // Add the calendar-iframe class
+          loading="lazy" // Add loading="lazy" for better page loading performance
+        ></iframe>
+      </div>
     </>
   );
 };
