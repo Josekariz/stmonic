0ce62ae59004d6d8a468c8483da30dc5b5466b38
Joseph Kariuki Macharia <sejokarizz@gmail.com>
2023-07-25 13:55:09 +0000

    add text to video background

--- src/homepage/VideoBackground.js
+++ src/homepage/VideoBackground.js
@@ -12,15 +12,14 @@ const VideoBackground = () => {
 
   return (
     <div className="video-container">
+      <div className="video-overlay"></div>
       {!videoError ? (
-        <video
-          src={vid}
-          autoPlay
-          muted
-          loop
-          onError={handleVideoError}
-        />
+        <video src={vid} autoPlay muted loop onError={handleVideoError} />
       ) : null}
+      <div className="text-container">
+        <h2 className="churchName">Ack stMonica Utawala</h2>
+        <p className="verse-text">Amos 5:14 - "Seek good, not evil, that you may live. Then the Lord God Almighty will be with you, just as you say he is."</p>
+      </div>
     </div>
   );
 };
