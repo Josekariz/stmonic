42993b1f9f8f7eebb006b5f91ea70d90c533d967
Joseph <sejokarizz@gmail.com>
2023-08-02 20:45:26 +0300

    add image and text layering on about us page

--- src/About.js
+++ src/About.js
@@ -2,7 +2,6 @@
 import React from "react";
 import Navbar1 from "./Nav/Navbar1";
 import img from "./homepage/hills.jpg";
-
 import "./AboutUs.css"; // We'll create this CSS file in the next step
 import Footer from "./Footer/Footer";
 
@@ -13,11 +12,11 @@ const AboutUs = () => {
       <div className="about-us">
         <div className="about-us-content">
           <div className="about-us-text">
-            <h2>Welcome to Our Company</h2>
+            <h2>stMonica</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit
-              neque in tellus volutpat, eu egestas ipsum luctus. Proin dictum felis
-              vel semper semper.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              hendrerit neque in tellus volutpat, eu egestas ipsum luctus. Proin
+              dictum felis vel semper semper.
             </p>
           </div>
           <div className="about-us-image">
@@ -31,18 +30,18 @@ const AboutUs = () => {
           <div className="about-us-text">
             <h2>Our Mission</h2>
             <p>
-              Nunc non tellus facilisis, pharetra nunc eget, facilisis ex. Integer
-              sit amet lacus eu metus commodo facilisis non vitae velit.
+              Nunc non tellus facilisis, pharetra nunc eget, facilisis ex.
+              Integer sit amet lacus eu metus commodo facilisis non vitae velit.
             </p>
           </div>
         </div>
         <div className="about-us-content">
           <div className="about-us-text">
-            <h2>Our Vision</h2>
+            <h2>We have a place for everyone in the church subgroups</h2>
             <p>
-              Phasellus eu sagittis purus. Aliquam a nisi velit. Integer eu dolor
-              metus. Ut lacinia ipsum nec justo ultricies, eget facilisis sapien
-              luctus.
+              Phasellus eu sagittis purus. Aliquam a nisi velit. Integer eu
+              dolor metus. Ut lacinia ipsum nec justo ultricies, eget facilisis
+              sapien luctus.
             </p>
           </div>
           <div className="about-us-image">
@@ -54,7 +53,7 @@ const AboutUs = () => {
             <img src={img} alt="" />
           </div>
           <div className="about-us-text">
-            <h2>Our Values</h2>
+            <h2>fellowships</h2>
             <p>
               Duis sit amet lacus nec turpis eleifend feugiat vel ac ex. Etiam
               rhoncus erat eu tellus porttitor volutpat.
@@ -63,7 +62,32 @@ const AboutUs = () => {
         </div>
         <div className="about-us-content">
           <div className="about-us-text">
-            <h2>Our Team</h2>
+            <h2>KAMA</h2>
+            <p>
+              Vivamus at ex in libero egestas tincidunt. Nulla sit amet felis ut
+              nunc pellentesque congue. Fusce sagittis nunc nunc, eget ultricies
+              arcu tincidunt eget.
+            </p>
+          </div>
+          <div className="about-us-image">
+            <img src={img} alt="" />
+          </div>
+        </div>
+        <div className="about-us-content">
+          <div className="about-us-image">
+            <img src={img} alt="" />
+          </div>
+          <div className="about-us-text">
+            <h2>MOthers union</h2>
+            <p>
+              Duis sit amet lacus nec turpis eleifend feugiat vel ac ex. Etiam
+              rhoncus erat eu tellus porttitor volutpat.
+            </p>
+          </div>
+        </div>
+        <div className="about-us-content">
+          <div className="about-us-text">
+            <h2>BLAZE youth</h2>
             <p>
               Vivamus at ex in libero egestas tincidunt. Nulla sit amet felis ut
               nunc pellentesque congue. Fusce sagittis nunc nunc, eget ultricies
@@ -75,7 +99,12 @@ const AboutUs = () => {
           </div>
         </div>
       </div>
-      <Footer/>
+      <div className="centered-text">
+        <p style={{ fontFamily: "Roboto, sans-serif", fontSize: "24px" }}>
+          All are welcome and loved here 😊
+        </p>
+      </div>
+      <Footer />
     </>
   );
 };
