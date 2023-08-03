a639918a7708751b661d5ebf5873f8e5c2e0f587
Joseph <sejokarizz@gmail.com>
2023-08-03 21:29:38 +0300

    create contact us page with slide right to left animation

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,18 +1,44 @@
+// ContactUs.js
 import React from "react";
-import Navbar1 from "./Nav/Navbar1";
+import { FaFacebook, FaInstagram, FaPhone, FaEnvelope, FaYoutube, FaPhoneAlt } from "react-icons/fa";
+import "./ContactUs.css";
+import Navbar from "./Nav/Navbar1";
+import Footer from "./Footer/Footer";
 
-
-const Contactus = () => {
-
-
+const ContactUs = () => {
   return (
     <>
-      <Navbar1 />
-      <h1>Welcome to the Contactus Page</h1>
 
-      <p>these are our office hours</p>
+    <Navbar/>
+    <div className="contact-container">
+    <div className="contact-card">
+      <h2>Contact Us</h2>
+      <p>We are open every day from Monday to Sunday, from 8 AM to 5 PM.</p>
+      <div className="contact-icons">
+        <a href="https://www.facebook.com/yourfacebookpage" target="_blank" rel="noopener noreferrer">
+          <FaFacebook className="icon" />
+        </a>
+        <a href="https://www.instagram.com/yourinstagram" target="_blank" rel="noopener noreferrer">
+          <FaInstagram className="icon" />
+        </a>
+        <a href="tel:+1234567890">
+          <FaPhone className="icon" />
+        </a>
+        <a href="mailto:info@example.com">
+          <FaEnvelope className="icon" />
+        </a>
+        <a href="https://www.youtube.com/youryoutubechannel" target="_blank" rel="noopener noreferrer">
+          <FaYoutube className="icon" />
+        </a>
+        <a href="tel:+0987654321">
+          <FaPhoneAlt className="icon" />
+        </a>
+      </div>
+    </div>
+    </div>
+    <Footer/>
     </>
   );
 };
 
-export default Contactus;
+export default ContactUs;
