07c05c6a7b467308f2ad5545d990b20751b5c9bb
Joseph <sejokarizz@gmail.com>
2023-08-05 18:06:56 +0300

    dry code for about us page

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -9,32 +9,25 @@ const AboutUs = () => {
     <>
       <Navbar />
       <div className="about-us">
-        <div className="about-us-content">
+        <div className="about-us-content-1">
           <div className="about-us-text">
-            <h2>stMonica</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              hendrerit neque in tellus volutpat, eu egestas ipsum luctus. Proin
-              dictum felis vel semper semper.
+            <h2>st Monica Church Utawala</h2>
+            <p className="about-us-text-content">
+              As an Anglican church, we are deeply rooted in the teachings of
+              God’s word and strive to uplift the lives of our community. We
+              openly preach the truth of God’s word and serve as a shining
+              beacon of hope and guidance, not only to Kenya but to the world at
+              large. Our mission is to spread the love and light of God to all
+              who seek it, and to provide a welcoming and supportive community
+              for all who wish to join us on our journey of faith.
             </p>
           </div>
           <div className="about-us-image">
             <img src={img} alt="" />
           </div>
         </div>
-        <div className="about-us-content">
-          <div className="about-us-image">
-            <img src={img} alt="" />
-          </div>
-          <div className="about-us-text">
-            <h2>Our Mission</h2>
-            <p>
-              Nunc non tellus facilisis, pharetra nunc eget, facilisis ex.
-              Integer sit amet lacus eu metus commodo facilisis non vitae velit.
-            </p>
-          </div>
-        </div>
-        <div className="about-us-content">
+
+        <div className="about-us-content-2">
           <div className="about-us-text">
             <h2>We have a place for everyone in the church subgroups</h2>
             <p>
@@ -47,50 +40,59 @@ const AboutUs = () => {
             <img src={img} alt="" />
           </div>
         </div>
-        <div className="about-us-content">
-          <div className="about-us-image">
-            <img src={img} alt="" />
-          </div>
+
+        {/* more */}
+        <div className="about-us-content-1">
           <div className="about-us-text">
-            <h2>fellowships</h2>
-            <p>
-              Duis sit amet lacus nec turpis eleifend feugiat vel ac ex. Etiam
-              rhoncus erat eu tellus porttitor volutpat.
-            </p>
-          </div>
-        </div>
-        <div className="about-us-content">
-          <div className="about-us-text">
-            <h2>KAMA</h2>
-            <p>
-              Vivamus at ex in libero egestas tincidunt. Nulla sit amet felis ut
-              nunc pellentesque congue. Fusce sagittis nunc nunc, eget ultricies
-              arcu tincidunt eget.
+            <h2>Fellowships</h2>
+            <p className="about-us-text-content">
+              As an Anglican church, we are deeply rooted in the teachings of
+              God’s word and strive to uplift the lives of our community. We
+              openly preach the truth of God’s word and serve as a shining
+              beacon of hope and guidance, not only to Kenya but to the world at
+              large. Our mission is to spread the love and light of God to all
+              who seek it, and to provide a welcoming and supportive community
+              for all who wish to join us on our journey of faith.
             </p>
           </div>
           <div className="about-us-image">
             <img src={img} alt="" />
           </div>
         </div>
-        <div className="about-us-content">
+
+        <div className="about-us-content-2">
+          <div className="about-us-text">
+            <h2>Blaze Youth.</h2>
+            <p>
+              Phasellus eu sagittis purus. Aliquam a nisi velit. Integer eu
+              dolor metus. Ut lacinia ipsum nec justo ultricies, eget facilisis
+              sapien luctus.
+            </p>
+          </div>
           <div className="about-us-image">
             <img src={img} alt="" />
           </div>
+        </div>
+
+        {/* more  */}
+
+        <div className="about-us-content-1">
           <div className="about-us-text">
-            <h2>MOthers union</h2>
-            <p>
-              Duis sit amet lacus nec turpis eleifend feugiat vel ac ex. Etiam
-              rhoncus erat eu tellus porttitor volutpat.
-            </p>
+            <h2>KAMA.</h2>
+            <p className="about-us-text-content">Mens associatio</p>
+          </div>
+          <div className="about-us-image">
+            <img src={img} alt="" />
           </div>
         </div>
-        <div className="about-us-content">
+
+        <div className="about-us-content-2">
           <div className="about-us-text">
-            <h2>BLAZE youth</h2>
+            <h2>Mothers Union. </h2>
             <p>
-              Vivamus at ex in libero egestas tincidunt. Nulla sit amet felis ut
-              nunc pellentesque congue. Fusce sagittis nunc nunc, eget ultricies
-              arcu tincidunt eget.
+              Phasellus eu sagittis purus. Aliquam a nisi velit. Integer eu
+              dolor metus. Ut lacinia ipsum nec justo ultricies, eget facilisis
+              sapien luctus.
             </p>
           </div>
           <div className="about-us-image">
