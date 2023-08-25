79c65818cd666b78f498b8e7843267df53bf15cc
Joseph Kariuki Macharia <sejokarizz@gmail.com>
2023-08-25 13:17:49 +0000

    add routes to new pages

--- src/BaseRoutes.jsx
+++ src/BaseRoutes.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom"
-import Home from "./homepage/Home.jsx";
-import Events from "./Events/Events.jsx";
-import About from "./About/About.jsx";
-import Contactus from "./contactus/ContactUs.jsx";
+import Home from "./homepage/Home";
+import Events from "./Events/Events";
+import About from "./About/About";
+import Contactus from "./contactus/ContactUs";
+import StMonica from "./About/Stmonica";
+import Subgroups from "./About/Subgroups";
+import Fellowships from "./About/Fellowship";
+import MensAssoc from "./About/MensAssoc";
+import Blaze from "./About/Blaze";
+import Mothers from "./About/Mothers";
 
 const BaseRouter = () => {
   return (
@@ -12,6 +18,12 @@ const BaseRouter = () => {
       <Route path="/events" element={<Events />} />
       <Route path="/about" element={<About />} />
       <Route path="/contactUs" element={<Contactus />} />
+      <Route path="/st-monica" element={<StMonica/>}></Route>
+      <Route path="/subgroups" element={<Subgroups/>}></Route>
+      <Route path="/fellowships" element={<Fellowships/>}></Route>
+      <Route path="/blaze" element={<Blaze/>}></Route>
+      <Route path="/kama" element={<MensAssoc/>}></Route>
+      <Route path="/mothers" element={<Mothers/>}></Route>
     </Routes>
   );
 };
