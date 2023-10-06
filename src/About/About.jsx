769b5d8c17a83a6237273c5b616456b4f2ae1dcc
Joseph Macharia <sejokarizz@gmail.com>
2023-10-06 13:22:35 +0000

    add assets for demo Carousel

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "../Nav/Navbar.jsx";
 import img from "../homepage/hills.jpg";
 import "./AboutUs.css";
--- src/About/Stmonica.jsx
+++ src/About/Stmonica.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import img1 from "./images/1.jpg"
+import img2 from "./images/2.jpg"
+import img3 from "./images/3.jpg"
+import img4 from "./images/4.jpg"
+import img5 from "./images/5.jpg"
+
+const images = [
+    { id: 1, src: img1, alt: 'Image 1' },
+    { id: 2, src: img2, alt: 'Image 2' },
+    { id: 3, src: img3, alt: 'Image 3' },
+    { id: 4, src: img4, alt: 'Image 4' },
+    { id: 5, src: img5, alt: 'Image 5' }
+];
 
 
 const StMonica = () => {
-    return (<>"this is the stMonica history page"</>);
+
+
+    return (<>"this is the stMonica history page"
+        <img src={img1} alt="" />
+    </>);
+
 }
 
 export default StMonica;
